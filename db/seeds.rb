b227fd225cd15e67860ec5ad9197074d96d18dca
zapbuild <gurmindersingh@zapbuild.com>
2020-03-05 14:14:30 +0530

    added seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+if Channel.count == 0
+  channel =  Channel.create({ name: 'Discovery'})
+  Show.create({name: 'Animal Safari' , channel_id: channel.id})
+
+  channel2 =  Channel.create({ name: 'Star Movies'})
+  Show.create({name: 'Transformers' , channel_id: channel2.id})
+
+end
+
